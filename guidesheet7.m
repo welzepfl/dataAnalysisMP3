8ef494abc08ee982c7a34890c3a1dac6a1c14663
welzepfl <roman.welz@epfl.ch>
2017-11-20 17:49:35 +0100

    first commit
    data and first few lines of code

---
+++ guidesheet7.m
@@ -0,0 +1,11 @@
+% Miniproject III
+% Guidesheet VII
+
+% some initial code
+addpath([pwd,'/functions']);
+set(0,'DefaultAxesFontSize',14);
+p_flag = 0; % exports figures only if set to 1
+
+data = load('Data.mat');
+
+%% 
